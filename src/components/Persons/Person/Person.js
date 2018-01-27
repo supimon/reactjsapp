82416ccd2ac34c6a318d14c58333a71db3412947
Supi <supimon@gmail.com>
2018-01-27 16:13:46 +0530

    changed Persons and Person to class components to study component lifecycle hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const Person = (props) => {
-
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.delete}>Hi I'm {props.name} and I am {props.age} yrs old.</p>
-      <p>{props.children}</p>
-      <input onChange={props.change} value={props.name}/>
-    </div>
-  );
-};
+class Person extends Component{
+  render(){
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.delete}>Hi I'm {this.props.name} and I am {this.props.age} yrs old.</p>
+        <p>{this.props.children}</p>
+        <input onChange={this.props.change} value={this.props.name}/>
+      </div>
+    );
+  }
+}
 
 export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,23 @@
 /**
  * Created by supimon on 26/01/18.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = props => props.persons.map((person, index) => {
-  return (
-    <Person
-      name={person.name}
-      age={person.age}
-      delete={props.deleted.bind(this, index)}
-      change={(event) => props.changed(event, person.id)}
-      key={person.id}
-    />
-  )
-});
+class Persons extends Component {
+  render(){
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          age={person.age}
+          delete={this.props.deleted.bind(this, index)}
+          change={(event) => this.props.changed(event, person.id)}
+          key={person.id}
+        />
+      )
+    })
+  }
+}
 
 export default Persons;
